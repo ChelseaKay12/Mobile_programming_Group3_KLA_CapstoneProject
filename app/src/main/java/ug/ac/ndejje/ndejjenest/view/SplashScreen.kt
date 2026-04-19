b53ad2c1e188b2f805949ffe465c440e1be596ec
= <=>
2026-04-19 22:10:26 +0300

    implemented Feature 3 App Title (NdejjeNest)

--- app/src/main/java/ug/ac/ndejje/ndejjenest/view/SplashScreen.kt
+++ app/src/main/java/ug/ac/ndejje/ndejjenest/view/SplashScreen.kt
@@ -2,16 +2,20 @@ package ug.ac.ndejje.ndejjenest.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ug.ac.ndejje.ndejjenest.R
 import ug.ac.ndejje.ndejjenest.ui.theme.NdejjeNestTheme
 import ug.ac.ndejje.ndejjenest.ui.theme.PrimaryDarkBlue
+import ug.ac.ndejje.ndejjenest.ui.theme.PrimaryYellow
 
 @Composable
 fun SplashScreen(onSplashFinished: () -> Unit) {
@@ -19,11 +23,14 @@ fun SplashScreen(onSplashFinished: () -> Unit) {
         modifier = Modifier.fillMaxSize(),
         color = PrimaryDarkBlue
     ) {
-        Box(
+        Column(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             LogoSection()
+            Spacer(modifier = Modifier.height(16.dp))
+            TitleSection()
         }
     }
 }
@@ -35,6 +42,22 @@ fun LogoSection() {
         contentDescription = "App Logo",
         modifier = Modifier.size(200.dp)
     )
+}
+
+@Composable
+fun TitleSection() {
+    Row {
+        Text(
+            text = "Ndejje",
+            style = MaterialTheme.typography.displayLarge,
+            color = Color.White
+        )
+        Text(
+            text = "Nest",
+            style = MaterialTheme.typography.displayLarge,
+            color = PrimaryYellow
+        )
+    }
 }
 
 @Preview(showBackground = true)
