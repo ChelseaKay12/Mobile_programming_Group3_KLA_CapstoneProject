3596884af02c6ce7a62b79e8012d7b9ce01fcdfb
MUHEREZA-JOSEPHAT <muherezajosephat2@gmail.com>
2026-04-21 22:02:21 +0300

    implemented_Feature 2_The Branding Logo

--- app/src/main/java/ug/ac/ndejje/ndejjenest/view/SplashScreen.kt
+++ app/src/main/java/ug/ac/ndejje/ndejjenest/view/SplashScreen.kt
@@ -1,10 +1,15 @@
 package ug.ac.ndejje.ndejjenest.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ug.ac.ndejje.ndejjenest.R
 import ug.ac.ndejje.ndejjenest.ui.theme.NdejjeNestTheme
 import ug.ac.ndejje.ndejjenest.ui.theme.PrimaryDarkBlue
 
@@ -14,8 +19,22 @@ fun SplashScreen(onSplashFinished: () -> Unit) {
         modifier = Modifier.fillMaxSize(),
         color = PrimaryDarkBlue
     ) {
-        // Content will be added in subsequent features
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            LogoSection()
+        }
     }
+}
+
+@Composable
+fun LogoSection() {
+    Image(
+        painter = painterResource(id = R.drawable.ndejjenest),
+        contentDescription = "App Logo",
+        modifier = Modifier.size(200.dp)
+    )
 }
 
 @Preview(showBackground = true)
